255a0c877de4975fec6fbdc10e40f8e9d65703b6
Juan Pablo Formento <juan_pablo94@outlook.com>
2022-08-31 18:56:49 -0300

    alta terminada

--- 3-BaseDeDatos.pas
+++ 3-BaseDeDatos.pas
@@ -19,15 +19,15 @@ type
 var
   archivo: text;
   meta_data: MetaData;
+  meta_size: integer;
+  NombreArchivo: string;
   
 {----------------------Funciones y procedimientos----------------------}
 
-//Crear Base de datos
+{-----------------Crear Base de datos-----------------}
 procedure crearBaseDeDatos();
 var
-	NombreArchivo: string;
 	meta_size_str: String[META_COUNT_SIZE];
-	meta_size: integer;
 	i: integer;
 	s: string;
 begin
@@ -59,7 +59,7 @@ begin
 			writeln('');
 			inc(i);
 		end;
-		
+		clrscr;
 		{Creo el archivo con el nombre que ingreso el usuario}
 		Assign(archivo, NombreArchivo);
 		rewrite(archivo);
@@ -75,14 +75,15 @@ begin
 				inc(i);
 			end;
 		close(archivo);
+		writeln('');
+		writeln('Base de datos creada con exito!!!');
+		readln;
 end;
 
-//Abrir Base de Datos
+{-----------------Abrir Base de Datos-----------------}
 procedure abrirBaseDeDatos();
 var
-	NombreArchivo: string;
 	meta_size_str: String[META_COUNT_SIZE];
-	meta_size: integer;
 	i: integer;
 	s: string;
 begin
@@ -100,34 +101,72 @@ begin
 	meta_size:= StrToInt(meta_size_str);
 	setLength(meta_data, meta_size);
 	
-	i:= 0;
-	
-	
-end;
-
-//Lee el archivo y lo muestra por pantalla
-{procedure leerArchivo(var archivo:text; NombreArchivo:string);
-var
-	i: integer;
-begin
-	reset(archivo);
-	write('Cantidad de campos: ');
-	writeln(meta_size);
-	writeln('');
-	
+	{Lee los metadatos del archivo, y los copia en el arreglo de metadata}
 	i:= 0;
 	while (i < meta_size) do
 		begin
-			writeln(i);
-			writeln(meta_data[i].name);
-			writeln(meta_data[i].size);
+			readln(archivo, s);
+			meta_data[i].name:= copy(s, 1, META_NAME_SIZE);
+			meta_data[i].size:= copy(s, 1 + META_NAME_SIZE, META_SIZE_SIZE);
 			inc(i);
 		end;
-	Close(archivo);
-	readkey;
-end;}
+	close(archivo);
+	
+	{Muestra los metadatos por pantalla}
+	write('Cantidad de campos: ');
+	writeln(meta_size);
+	writeln('');	
+	i:= 0;
+	while (i < meta_size) do
+		begin
+			write(i);
+			write('		');
+			write(meta_data[i].name);
+			write('		');
+			write(meta_data[i].size);
+			writeln('');
+			inc(i);
+		end;
+	readln;
+end;
 
-//Menu principal
+{-----------------Procedimiento que agrega un registro al archivo-----------------}
+procedure alta();
+var
+	s: string;
+	i, size: integer;
+begin
+	assign(archivo, NombreArchivo);
+	append(archivo);
+	writeln('Nombre de la base de datos: ' + NombreArchivo);
+	writeln('');
+	i:= 0;
+	{Agrega un enter al archivo}
+	write(archivo, char (13));
+	while i < meta_size do
+		begin
+			writeln('Ingrese ' + meta_data[i].name);
+			readln(s);
+			size:= StrToInt(meta_data[i].size);
+			{Si el texto ingresado es mayor al declarado en la meta data
+			* lo recorta hasta el maximo de la metadata}
+			if (Length(s) > size) then
+				begin
+					write(archivo, Copy(s,1,size));
+				end
+			{Si es menor completo con espacios en blanco hasta 
+			* el maximo declarado en la meta data}
+			else
+				begin
+					write(archivo, PadRight(s, size));
+				end;
+			inc(i);
+		end;
+	close(archivo);
+	readln;
+end;
+
+{-----------------Menu principal-----------------}
 procedure menu(var archivo:text);
 var
 	opcion:char;
@@ -139,13 +178,14 @@ begin
 		textcolor(15);
 		gotoxy(2,3);writeln('1- Crear Base de datos');
 		gotoxy(2,5);writeln('2- Abrir Base de datos');
-		gotoxy(2,7);writeln('3- Alta');
-		gotoxy(2,9);writeln('4- Baja');
-		gotoxy(2,11);writeln('5- Modificacion');
+		gotoxy(2,7);writeln('3- Ver archivo');
+		gotoxy(2,9);writeln('4- Alta');
+		gotoxy(2,11);writeln('5- Baja');
+		gotoxy(2,13);writeln('6- Modificacion');
 		textcolor(12);
-		gotoxy(2,13);writeln('0- Salir');
+		gotoxy(2,15);writeln('0- Salir');
 		textcolor(45);
-		gotoxy(2,15);writeln('Elija una opcion');
+		gotoxy(2,17);writeln('Elija una opcion');
 		textcolor(15);
 		repeat
 			opcion:=readkey;
@@ -155,12 +195,14 @@ begin
 			'1':crearBaseDeDatos();
 			'2':abrirBaseDeDatos();
 			'3':;
-			'4':;
+			'4':alta();
 			'5':;
+			'6':;
 		end;
 	until opcion='0';
 end;	
 
+{-----------------App-----------------}
 BEGIN
 	setLength(meta_data,0);
 	menu(archivo);
