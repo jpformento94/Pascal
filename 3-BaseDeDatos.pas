60288eecdb0d5073fd786301f39d5e35bd9d3bdc
Juan Pablo Formento <juan_pablo94@outlook.com>
2022-09-08 17:53:51 -0300

    Base de Datos terminada

--- 3-BaseDeDatos.pas
+++ 3-BaseDeDatos.pas
@@ -61,7 +61,7 @@ begin
 		end;
 		clrscr;
 		{Creo el archivo con el nombre que ingreso el usuario}
-		Assign(archivo, NombreArchivo);
+		Assign(archivo, NombreArchivo + '.txt');
 		rewrite(archivo);
 		writeln(archivo, PadRight(meta_size_str, META_COUNT_SIZE));
 		
@@ -91,7 +91,7 @@ begin
 	{Solicita al usuario que ingrese el nombre del archivo de texto}
 	write('Ingrese el nombre del archivo: ');
 	readln(NombreArchivo);
-	Assign(archivo, NombreArchivo);
+	Assign(archivo, NombreArchivo + '.txt');
 	reset(archivo);
 	
 	{Lee la primera linea del archivo y se guarda el valor que indica
@@ -134,31 +134,37 @@ end;
 procedure mostrar();
 var
 	s: string;
-	i, j: integer;
+	i, j,p : integer;
 begin
 	reset(archivo);
 	i:= 0;
 	j:= 11;
+	p:= 3;
 	s:= '';
 	{Muestra el nombre de las columnas}
 	textcolor(10);
+	gotoxy(p,1);
 	while (i < meta_size) do
 		begin
 			write(meta_data[i].name);
-			gotoxy(i + j, 1);
+			gotoxy(p + j, 1);
 			j:= j + 9;
 			inc(i);
 		end;
 	writeln('');
 	{Muestra el contenido del archivo}
 	i:= 0;
+	p:= 1;
 	textcolor(15);
 	while not eof(archivo) do
 		begin
 			if (i > meta_size) then
 				begin
 					readln(archivo,s);
+					write(p);
+					write(' ');
 					writeln(s);
+					inc(p);
 				end
 			else
 				begin
@@ -202,6 +208,151 @@ begin
 	{Agrega un enter al archivo}
 	write(archivo, char (13));
 	close(archivo);
+	writeln('');
+	writeln('Agregado con exito!');
+	readln;
+end;
+
+{-----------------Procedimiento que borra un registro del archivo-----------------}
+procedure baja();
+var
+	archivoaux: textfile;
+	pos_eliminar, pos_real: integer;
+	i, meta_total: integer;
+	s: string;
+	opcion: char;
+begin
+	clrscr;
+	{cantidad de campos que forman parte de la metadata en total}
+	meta_total:= meta_size + 1;
+
+	reset(archivo);
+	assign(archivoaux, 'auxiliar.txt');
+	rewrite(archivoaux);
+	writeln('Indice que la posicion que desea borrar');
+	{esta es la posicion que ve el usuario}
+	readln(pos_eliminar);
+	
+	{esta es la posicion real en el archivo, se compone de
+	* la que ve el usuario mas los campos de la metadata}
+	pos_real:= meta_total + pos_eliminar;
+	
+	{leo y guardo en el archivo auxiliar hasta una posicion antes de la
+	* que el usuario quiere eliminar}
+	for i:= 1 to (pos_real - 1) do
+		begin
+			readln(archivo, s);
+			writeln(archivoaux, s);
+		end;
+	
+	{leo la siguiente posicion que se corresponde con la que el usuario
+	* solicito eliminar}
+	readln(archivo,s);
+
+	clrscr;
+	write('Esta a punto de borrar a ');
+	TextColor(green);
+	write(s);
+	writeln('');
+	writeln('');
+	TextColor(red);
+	write('Esta seguro? (S/N)');
+	TextColor(15);
+	readln(opcion);
+	if (opcion = 's') or (opcion = 'S') then
+		begin
+			while not eof(archivo) do
+				begin
+					readln(archivo, s);
+					writeln(archivoaux, s);
+					
+				end;
+			close(archivo);
+			erase(archivo);
+			close(archivoaux);
+			rename(archivoaux, NombreArchivo + '.txt');
+			clrscr;
+			writeln('Registro eliminado con exito!');
+			readln;
+		end
+	else
+		begin
+			clrscr;
+			writeln('No se elimino ningun registro');
+			readln;
+			close(archivo);
+			close(archivoaux);
+		end;
+end;
+
+{-----------------Procedimiento que modifica un registro del archivo-----------------}
+procedure modificar();
+var
+	archivoaux: textfile;
+	pos_modificar, pos_real: integer;
+	i, meta_total, size: integer;
+	s: string;
+begin
+	clrscr;
+	{cantidad de campos que forman parte de la metadata en total}
+	meta_total:= meta_size + 1;
+
+	reset(archivo);
+	assign(archivoaux, 'auxiliar.txt');
+	rewrite(archivoaux);
+	writeln('Indice que la posicion que desea modificar');
+	{esta es la posicion que ve el usuario}
+	readln(pos_modificar);
+	
+	{esta es la posicion real en el archivo, se compone de
+	* la que ve el usuario mas los campos de la metadata}
+	pos_real:= meta_total + pos_modificar;
+	
+	{leo y guardo en el archivo auxiliar hasta una posicion antes de la
+	* que el usuario quiere modificar}
+	for i:= 1 to (pos_real - 1) do
+		begin
+			readln(archivo, s);
+			writeln(archivoaux, s);
+		end;
+	
+	{la modificacion se efectua como un proceso de alta}
+	i:= 0;
+	while i < meta_size do
+		begin
+			writeln('Ingrese ' + meta_data[i].name);
+			readln(s);
+			size:= StrToInt(Trim(meta_data[i].size));
+			{Si el texto ingresado es mayor al declarado en la meta data
+			* lo recorta hasta el maximo de la metadata}
+			if (Length(s) > size) then
+				begin
+					write(archivoaux, Copy(s,1,size));
+				end
+			{Si es menor completo con espacios en blanco hasta 
+			* el maximo declarado en la meta data}
+			else
+				begin
+					write(archivoaux, PadRight(s, size));
+				end;
+			inc(i);
+		end;
+	{Agrega un enter al archivo}
+	write(archivoaux, char (13));
+
+	readln(archivo, s);
+	
+	while not eof(archivo) do
+		begin
+			readln(archivo, s);
+			writeln(archivoaux, s);	
+		end;
+	close(archivo);
+	erase(archivo);
+	close(archivoaux);
+	rename(archivoaux, NombreArchivo + '.txt');
+	clrscr;
+	writeln('Registro modificado con exito!');
 	readln;
 end;
 
@@ -228,15 +379,15 @@ begin
 		textcolor(15);
 		repeat
 			opcion:=readkey;
-		until opcion in ['0'..'5'];
+		until opcion in ['0'..'6'];
 		clrscr;
 		case opcion of
 			'1':crearBaseDeDatos();
 			'2':abrirBaseDeDatos();
 			'3':mostrar();
 			'4':alta();
-			'5':;
-			'6':;
+			'5':baja();
+			'6':modificar();
 		end;
 	until opcion='0';
 end;	
