b02026a670576581d5acf32000fa969a69437197
Juan Pablo Formento <juan_pablo94@outlook.com>
2022-09-09 15:46:26 -0300

    crea archivo, alta y ver archivo

--- 1-ABM.pas
+++ 1-ABM.pas
@@ -1,22 +1,23 @@
 program ABM;
 
 uses
-	crt;
+	crt, SysUtils;
 
 type
-	cliente = record
+	TipoCliente = record
 		apellido: string[20];
 		nombre: string[20];
 		numero: integer;
+		estado: boolean;
 	end;
 
-	bd = file of cliente;
+	Archivo = file of TipoCliente;
 	
 var 
-	archivo: bd;
+	a: Archivo;
 
 {-----------------Abre el archivo y si no existe lo crea-----------------}
-procedure abrir (var f: bd);
+procedure abrir (var f: archivo);
 var
 	resultado : Integer;
 begin
@@ -26,254 +27,95 @@ begin
 	{$I+}
 	if resultado <> 0 then
 		rewrite (f);
-	rlose (f);
+	close (f);
 end;
 
 {-----------------Procedimiento que agrega un registro al archivo-----------------}
-procedure alta (var f :bd);
-var
-	R, E : Estudiante;
-	I	 : Integer;		{posicion del registro en el archivo}
-
-begin
-	Clrscr;
-	Gotoxy(25, 2); Writeln ('Altas');
-	Reset (F);
-	Gotoxy (1,24); Writeln ('Presionar N para salir');
-	while Upcase (readkey) <> 'N' do
-		begin
-        	gotoxy (20, 13);
-			writeln ('Introducir los datos del nuevo registro');
-			LeerRegistro (E);
-			I := Posicion (E.Apynom, F);
-			if I = -1 then              {el registro no existe}
-				begin
-					I := FileSize (F);
-					Seek (F, I);  {se posiciona al final del archivo}
-					Write (F, E);
-				end
-			else
-				begin
-					Seek (F, I);
-					Read (F, R);
-					if R.Sw then      {si existe, verifica que se
-	 								encuentre libre}
-						begin
-							Gotoxy (20, 21);
-							writeln ('Registro Existente');
-						end
-					else
-					    begin
-							seek(F,filepos(F) - 1);
-							write (F, E);
-						end;
-				end;
-			gotoxy (20, 21);
-			writeln ('Presionar N para salir')
-		end;
-    close (F)
-end;
-
 procedure alta();
-var
-	apellido, nombre: string;
-	cliente: integer;
+var 
+	cliente: TipoCliente;
+	pos: integer;
 begin
-	append(archivo);
 	clrscr;
+	
 	TextColor(10);
 	writeln('<=====Alta=====>');
+	writeln('');
+	
 	TextColor(15);
+	write('Ingrese el apellido: ');
+	readln(cliente.apellido);
 	writeln('');
-	writeln('Ingrese apellido');
-	readln(apellido);
+	
+	write('Ingrese el nombre: ');
+	readln(cliente.nombre);
 	writeln('');
-	writeln('Ingrese nombre');
-	readln(nombre);
-	writeln('');
-	writeln('Ingrese codigo de cliente');
-	readln(cliente);
-	while (cliente <= 0) or (cliente > 9999) do
+	
+	write('Ingrese el numero de cliente: ');
+	readln(cliente.numero);
+	while (cliente.numero <= 0) or (cliente.numero > 9999) do
 		begin
-			clrscr;
 			TextColor(red);
-			writeln('El codigo de cliente debe ser mayor a 0 y menor a 9999');
+			writeln('');
+			writeln('Error, el numero de cliente debe ser mayor a cero y de 4 digitos: ');
 			TextColor(15);
-			writeln('Ingrese el codigo de cliente nuevamente');
-			readln(cliente);
+			readln(cliente.numero);
 		end;
-	writeln(archivo, apellido + ' ', nombre + ' ', cliente);
-	close(archivo);
-	writeln('');
-	writeln('Agregado con exito!');
-	readln;
-end;
-
-{-----------------Procedimiento que borra un registro del archivo-----------------}
-procedure baja();
-var
-	archivoaux: textfile;
-	i: integer;
-	pos_eliminar: integer;
-	opcion: char;
-	s: string;
-begin
-	clrscr;
-
-	reset(archivo);
-	assign(archivoaux, 'auxiliar.txt');
-	rewrite(archivoaux);
-	writeln('Indique la posicion que desea borrar');
-	readln(pos_eliminar);
-	
-	{leo y guardo en el archivo auxiliar hasta una posicion antes de la
-	* que el usuario quiere eliminar}
-	for i:= 1 to (pos_eliminar - 1) do
-		begin
-			readln(archivo, s);
-			writeln(archivoaux, s);
-		end;
-	
-	{leo la siguiente posicion que se corresponde con la que el usuario
-	* solicito eliminar}
-	readln(archivo,s);
-
-	clrscr;
-	write('Esta a punto de borrar a ');
-	TextColor(green);
-	write(s);
-	writeln('');
-	writeln('');
-	TextColor(red);
-	write('Esta seguro? (S/N)');
+		
+	cliente.estado:= true;
 	TextColor(15);
-	readln(opcion);
-	if (opcion = 's') or (opcion = 'S') then
-		begin
-			while not eof(archivo) do
-				begin
-					readln(archivo, s);
-					writeln(archivoaux, s);
-				end;
-			close(archivo);
-			erase(archivo);
-			close(archivoaux);
-			rename(archivoaux, 'datos.txt');
-			clrscr;
-			writeln('Registro eliminado con exito!');
-			readln;
-		end
-	else
-		begin
-			clrscr;
-			writeln('No se elimino ningun registro');
-			readln;
-			close(archivo);
-			close(archivoaux);
-		end;
-end;
-
-{-----------------Procedimiento que modifica un registro del archivo-----------------}
-procedure modificar();
-var
-	archivoaux: textfile;
-	i: integer;
-	pos_modificar: integer;
-	s: string;
-	apellido, nombre: string;
-	cliente: integer;
-begin
-	clrscr;
-
-	reset(archivo);
-	assign(archivoaux, 'auxiliar.txt');
-	rewrite(archivoaux);
-	writeln('Indice que la posicion que desea modificar');
-	readln(pos_modificar);
-	
-	{leo y guardo en el archivo auxiliar hasta una posicion antes de la
-	* que el usuario quiere modificar}
-	for i:= 1 to (pos_modificar - 1) do
-		begin
-			readln(archivo, s);
-			writeln(archivoaux, s);
-		end;
-	
-	{la modificacion se efectua como un proceso de alta}
-	clrscr;
-	writeln('Ingrese apellido');
-	readln(apellido);
+	reset(a);
+	pos:= filesize(a);
+	seek(a,pos);
+	write(a,cliente);
+	close(a);
 	writeln('');
-	writeln('Ingrese nombre');
-	readln(nombre);
-	writeln('');
-	writeln('Ingrese codigo de cliente');
-	readln(cliente);
-	while (cliente <= 0) or (cliente > 9999) do
-		begin
-			clrscr;
-			TextColor(red);
-			writeln('El codigo de cliente debe ser mayor a 0 y menor a 9999');
-			TextColor(15);
-			writeln('Ingrese el codigo de cliente nuevamente');
-			readln(cliente);
-		end;
-	writeln(archivoaux, apellido + ' ', nombre + ' ', cliente);
-	
-	readln(archivo, s);
-	
-	while not eof(archivo) do
-		begin
-			readln(archivo, s);
-			writeln(archivoaux, s);	
-		end;
-	close(archivo);
-	erase(archivo);
-	close(archivoaux);
-	rename(archivoaux, 'datos.txt');
-	clrscr;
-	writeln('Registro modificado con exito!');
-	readln;
+	writeln('El registro se ha agregado exitosamente. Presione una tecla para volver al menu.');
+	readkey;
 end;
 
 {-----------------Procedimiento que muestra los registros cargados en el archivo-----------------}
 procedure mostrar();
 var
-	i, p : integer;
-	s: string;
+	cliente: TipoCliente;
+	p: integer;
 begin
-	reset(archivo);
-	{Muestra el nombre de las columnas}
+	clrscr;
+	reset(a);
 	textcolor(10);
 	gotoxy(4,1);
 	write('Apellido');
-	gotoxy(24,1);
+	gotoxy(25,1);
 	write('Nombre');
-	gotoxy(44,1);
+	gotoxy(45,1);
 	write('Cliente');
 	writeln('');
-	{Muestra el contenido del archivo}
-	i:= 2;
-	p:= 2;
 	textcolor(15);
-	while not eof(archivo) do
-		begin
-			readln(archivo,s);
-			write(p-1);
-			gotoxy(4,p);
-			writeln(s);
-			inc(p);
-			inc(i);
-		end;
-
-  GotoXY(20,10);
-  Writeln('Hola');
-
-	readln();
+	p:= 2;
+	while not eof(a) do begin
+		read(a,cliente);
+		if (cliente.estado = true) then 
+			begin	
+				gotoxy(1,p);
+				write(inttostr(FilePos(a)));
+				gotoxy(4,p);
+				write(cliente.apellido);
+				gotoxy(25,p);
+				write(cliente.nombre);
+				gotoxy(46,p);
+				write(IntToStr(cliente.numero));
+				writeln;
+				inc(p);
+			end;
+	end;
+	close(a);
+	writeln;
+	writeln('Presione una tecla para volver al menu.');
+	readkey;
 end;
 
 {-----------------Menu principal-----------------}
-procedure menu(var archivo:bd);
+procedure menu();
 var
 	opcion:char;
 begin
@@ -297,15 +139,15 @@ begin
 		clrscr;
 		case opcion of
 			'1':alta();
-			'2':baja();
-			'3':modificar();
+			'2':;//baja();
+			'3':;//modificar();
 			'4':mostrar();
 		end;
 	until opcion='0';
 end;	
 
 BEGIN
-	assign(archivo,'datos.dat');
-	abrir(archivo);
-	menu(archivo);
+	assign(a,'datos.dat');
+	abrir(a);
+	menu();
 END.
