467d6d4f33bcc32e279ca7b77ccc4400756d4137
Juan Pablo Formento <juan_pablo94@outlook.com>
2022-09-12 13:36:46 -0300

    agregar tiempo de reloj

--- Round Robin.pas
+++ Round Robin.pas
@@ -12,7 +12,7 @@ type
 		nombre: string;
 		ts:integer;		//tiempo de servicio
 		te:integer;		//tiempo de espera
-		tr:integer;		//tiempo de retorno
+		tr:integer;		//tiempo de reloj utilizado
 		ti:integer;		//tiempo de inicio
 		tsa:integer;	//tiempo de salida
 	end;
@@ -67,9 +67,14 @@ begin
 			for i:= MIN to cantidad_procesos do
 				begin
 					write('Nombre del proceso: ');write(LP[i].nombre);
-					write(' | ');
+					writeln;
+					writeln;
 					write('Tiempo de servicio inicial: ');write(LP[i].ti);
 					writeln;
+					writeln;
+					write('Tiempo de servicio restante: ');write(LP[i].ts);
+					writeln;
+					writeln('-----------------------------------------------');
 				end;
 		end;
 	readkey;
@@ -108,12 +113,53 @@ begin
 	Q:= i;
 end;
 
+{-----------------Sumatoria de los tiempos de los procesos-----------------}
+function sumatoriaTS():integer;
+var
+	contador, i: integer;
+begin
+	contador:= 0;
+	for i:= MIN to cantidad_procesos do
+		contador:= contador + LP[i].ts;
+	sumatoriaTS:= contador;
+end;
+
 {-----------------Round Robin-----------------}
 procedure roundRobin();
 var
-	i: integer;
+	i, vuelta: integer;
 begin
-	
+	vuelta:= 0;
+	{mientras la suma de los tiempos sea mayor que 0 proceso}
+	while (sumatoriaTS > 0) do
+		begin
+			{recorro la lista de procesos}
+			for i:= MIN to cantidad_procesos do
+				begin
+					{si el tiempo de servicio es mayor a 0, lo proceso}
+					if (LP[i].ts > 0) then
+						begin
+							{si es mayor a Q voy a necesitar al menos
+							* una vuelta mas para finalizarlo}
+							if (LP[i].ts > Q) then
+								begin
+									{le resto el Q al tiempo restante del proceso}
+									LP[i].ts:= LP[i].ts - Q;
+								end
+							else
+								{si no es mayor que Q, el proceso termina en esta ronda}
+								begin
+									LP[i].ts:= 0;
+								end;
+						end;
+				end;
+			inc(vuelta);
+			clrscr;
+			{muestro como estan los procesos al final de cada vuelta}
+			verProcesos();
+			write('Vuelta numero: ');write(vuelta);
+			readkey;
+		end;
 end;
 
 
@@ -130,12 +176,13 @@ begin
 		writeln('|                    Round Robin                        |');
 		writeln('|=======================================================|');
 		writeln;
+		gotoxy(2,5);
 		write('Quantum = ');write(Q);
 		textcolor(15);
 		gotoxy(2,7);writeln('1- Cargar procesos');
 		gotoxy(2,9);writeln('2- Mostrar procesos');
 		gotoxy(2,11);writeln('3- Reiniciar lista de procesos');
-		gotoxy(2,13);writeln('4- Indicar Quantum');
+		gotoxy(2,13);writeln('4- Cambiar Quantum');
 		gotoxy(2,15);writeln('5- Iniciar Round Robin');
 		textcolor(red);
 		gotoxy(2,17);writeln('0- Salir');
@@ -157,6 +204,7 @@ begin
 end;	
 
 BEGIN
+	Q:= 4;
 	menu();
 END.
 
